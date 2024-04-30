c84b973dea2bd7d1121ae970cffbbf331a791737
will <88606984+koronopizza@users.noreply.github.com>
2024-04-29 19:44:15 -0500

    working on varibles

--- python.py
+++ python.py
@@ -1,3 +1,9 @@
 # Python uses indentation to indicate a block of code.
 if 5 > 2:
   print("Five is greater than two!")
+  
+x = "hello"
+y = "world"
+print(x)
+print(y)
+# A variable is created the moment you first assign a value to it.
