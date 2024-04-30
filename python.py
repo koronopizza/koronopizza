0fc88476321936666f76e7e9ee6d9427b4f3da23
will <88606984+koronopizza@users.noreply.github.com>
2024-04-29 20:48:00 -0500

    Update python.py
    still working on my variables

--- python.py
+++ python.py
@@ -1,9 +1,25 @@
-# Python uses indentation to indicate a block of code.
 if 5 > 2:
+  #print is an output function
   print("Five is greater than two!")
+  # Python uses indentation to indicate a block of code.
   
 x = "hello"
 y = "world"
 print(x)
 print(y)
 # A variable is created the moment you first assign a value to it.
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+#If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+
+sandwich = ["bacon","lettuce","tomato"]
+x,y,z = sandwich
+print(x)
+print(y)
+print(z)
+#ex showed my work
+
