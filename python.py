e23f20690044dcce441babed840be25a493fc8b3
will <88606984+koronopizza@users.noreply.github.com>
2024-05-01 12:20:27 -0500

    Update python.py
    working on python numbers and castings

--- python.py
+++ python.py
@@ -53,5 +53,28 @@ print(x)
 print(type(x)) 
 #these are examples of python data types
 
+#---------------------------------------05-1-2024
+#convert from int to float:
+x = float(1)
 
+#convert from float to int:
+y = int(2.8)
 
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+#ex
+a = int(4.5)
+
+print(a)
+
+import random
+
+print(random.randrange(1, 1000))
