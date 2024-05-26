1d01e1ce35946efefceb6a996f1c1e1aa9f86144
will <88606984+koronopizza@users.noreply.github.com>
2024-05-25 20:46:53 -0500

    a few changes

--- python.py
+++ python.py
@@ -395,8 +395,20 @@ print(thistuple[-2])
 thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(thistuple[2:5])
 
-thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+thistuple = ("m", "a", "n", "g", "o")
 print(thistuple[1:4])
 
-#
+#Specify negative indexes if you want to start the search from the end of the tuple
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+thistuple = ("e", "a", "y", "g", "i", "n", "o")
+print(thistuple[-5:-2])
+
+#To determine if a specified item is present in a tuple use the in keyword
+thistuple = ("p", "a", "r")
+if "p" in thistuple:
+  print("Yes, 'p' is in the fruits tuple")
+  
+  
 
