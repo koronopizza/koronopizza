dda087b1c5bb4d1ba9a697cce8e6426365e2c0eb
will <88606984+koronopizza@users.noreply.github.com>
2024-06-07 17:37:49 -0500

    del

--- python.py
+++ python.py
@@ -429,4 +429,7 @@ y = list(thistuple)
 y.remove("car")
 thistuple = tuple(y)
 
-#
+#The del keyword can delete the tuple completely
+thistuple = ("e", "a", "y")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
